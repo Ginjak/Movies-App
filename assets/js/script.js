a1cd9359890d8b3deb681d108bf5b6984f208a84
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-01-17 18:35:05 +0000

    Updated APi keys and query url for NYT, Youtube, OMDB

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,28 @@
 /* OMDB Url: https://www.omdbapi.com/ 
 query url: http://www.omdbapi.com/?apikey=[yourkey]&
+OMDB query var querUrl = "http://www.omdbapi.com/?apikey=d3150aaf&t=The+matrix";
 OMDB API - d3150aaf
+
+Youtube API - AIzaSyAh-n-mfDEgD7pppdEFT1Mc8gflKClHvjw
+Youtube link to show video, need to add id of a video https://www.youtube.com/watch?v=
+var querUrl =
+  "https://www.googleapis.com/youtube/v3/search?part=snippet&q=Matrixtrailer&type=video&key=AIzaSyAh-n-mfDEgD7pppdEFT1Mc8gflKClHvjw";
 
 New York times url: https://developer.nytimes.com/
 Query to look for movie reviews:
 section_name:"Movies" AND type_of_material:"Review"
-
 https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=section_name%3A"Movies" AND type_of_material%3A"Review"&sort=newest&page=0&api-key{your-key}
+var querUrl =
+"https://api.nytimes.com/svc/search/v2/articlesearch.json?q=The%20Matrix&fq=section_name:Movies&type_of_material:Review&sort=newest&page=0&api-key=v7NMMpYkjMpqpGFtYZymGBQiWFEMTMEb";
+
 
 API - v7NMMpYkjMpqpGFtYZymGBQiWFEMTMEb
 */
 
 var querUrl =
-  "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=The%20Matrix&fq=section_name:Movies&type_of_material:Review&sort=newest&page=0&api-key=v7NMMpYkjMpqpGFtYZymGBQiWFEMTMEb";
+  "https://www.googleapis.com/youtube/v3/search?part=snippet&q=Matrixtrailer&type=video&key=AIzaSyAh-n-mfDEgD7pppdEFT1Mc8gflKClHvjw";
+
+// OMDB query var querUrl = "http://www.omdbapi.com/?apikey=d3150aaf&t=The+matrix";
 
 fetch(querUrl)
   .then((resp) => {
@@ -20,4 +30,16 @@ fetch(querUrl)
   })
   .then((data) => {
     console.log(data);
+    // Title - console.log(data.Title);
+    // Release yearconsole.log(data.Year);
+    // Runtime in minutes console.log(data.Runtime);
+    // Age rating console.log(data.Rated);
+    // ImdbRating console.log(data.imdbRating);
+    // Movie genreconsole.log(data.Genre);
+    // Movie plot console.log(data.Plot);
+    //Boxoffice new console.log(data.BoxOffice)
+    // Movie directors console.log(data.Director);
+    // Movie Actors console.log(data.Actors);
+    // Writers console.log(data.Writer);
+    // Poster image url console.log(data.Poster);
   });
