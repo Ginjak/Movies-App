7f51053c38baa0eba63b81505c0c76116c5da6ef
dudi62 <tdudek62@gmail.com>
2024-01-19 19:32:17 +0000

    add movieinfo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,89 @@
-/* OMDB Url: https://www.omdbapi.com/ 
-query url: http://www.omdbapi.com/?apikey=[yourkey]&
-OMDB query var querUrl = "http://www.omdbapi.com/?apikey=d3150aaf&t=The+matrix";
-OMDB API - d3150aaf
+// Initial References
+let movieNameRef = document.getElementById("movie-name");
+let searchBtn = document.getElementById("search-btn");
+let result = document.getElementById("result");
 
-Youtube API - AIzaSyAh-n-mfDEgD7pppdEFT1Mc8gflKClHvjw
-Youtube link to show video, need to add id of a video https://www.youtube.com/watch?v=
-var querUrl =
-  "https://www.googleapis.com/youtube/v3/search?part=snippet&q=Matrixtrailer&type=video&key=AIzaSyAh-n-mfDEgD7pppdEFT1Mc8gflKClHvjw";
+// Add your YouTube Data API key here
+const youtubeKey = 'AIzaSyAh-n-mfDEgD7pppdEFT1Mc8gflKClHvjw';
 
-New York times url: https://developer.nytimes.com/
-Query to look for movie reviews:
-section_name:"Movies" AND type_of_material:"Review"
-https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=section_name%3A"Movies" AND type_of_material%3A"Review"&sort=newest&page=0&api-key{your-key}
-var querUrl =
-"https://api.nytimes.com/svc/search/v2/articlesearch.json?q=The%20Matrix&fq=section_name:Movies&type_of_material:Review&sort=newest&page=0&api-key=v7NMMpYkjMpqpGFtYZymGBQiWFEMTMEb";
+// Function to fetch YouTube videos related to the movie
+let getYoutubeVideos = (movieName) => {
+    let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${movieName} trailer&key=${youtubeKey}`;
+  
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        // If videos exist
+        if (data.items.length > 0) {
+          let videosHtml = '<h3>Trailer:</h3>';
+  
+          videosHtml += `
+            <iframe width="540" height="315" src="https://www.youtube.com/embed/${data.items[0].id.videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          `;
+  
+          result.innerHTML += videosHtml;
+        }
+      })
+      .catch(() => {
+        result.innerHTML += '<h3 class="msg">Error Occurred While Fetching YouTube Videos</h3>';
+      });
+  };
 
+// Function to fetch data from API
+const key = '2bd71b72';
+let getMovie = () => {
+  let movieName = movieNameRef.value;
+  let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+  // If input field is empty
+  if (movieName.length <= 0) {
+    result.innerHTML = `<h3 class="msg">Please Enter A Movie Name</h3>`;
+  }
+  // If input field is NOT empty
+  else {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        // If movie exists in database
+        if (data.Response == "True") {
+          result.innerHTML = `
+            <div class="info">
+                <img src=${data.Poster} class="poster">
+                <div>
+                    <h2>${data.Title}</h2>
+                    <div class="rating">
+                    <span style="font-size:300%;color:yellow;">&starf;</span>
+                        <h4>${data.imdbRating}</h4>
+                    </div>
+                    <div class="details">
+                        <span>${data.Rated}</span>
+                        <span>${data.Year}</span>
+                        <span>${data.Runtime}</span>
+                    </div>
+                    <div class="genre">
+                        <div>${data.Genre.split(",").join("</div><div>")}</div>
+                    </div>
+                </div>
+            </div>
+            <h3>Plot:</h3>
+            <p>${data.Plot}</p>
+            <h3>Cast:</h3>
+            <p>${data.Actors}</p>
+          `;
 
-API - v7NMMpYkjMpqpGFtYZymGBQiWFEMTMEb
-*/
+          // Fetch YouTube videos related to the movie
+          getYoutubeVideos(movieName);
+        }
+        // If movie does NOT exists in database
+        else {
+          result.innerHTML = `<h3 class='msg'>${data.Error}</h3>`;
+        }
+      })
+      // If error occurs
+      .catch(() => {
+        result.innerHTML = `<h3 class="msg">Error Occurred</h3>`;
+      });
+  }
+};
 
-var querUrl =
-  "https://www.googleapis.com/youtube/v3/search?part=snippet&q=Matrixtrailer&type=video&key=AIzaSyAh-n-mfDEgD7pppdEFT1Mc8gflKClHvjw";
-
-// OMDB query var querUrl = "http://www.omdbapi.com/?apikey=d3150aaf&t=The+matrix";
-
-fetch(querUrl)
-  .then((resp) => {
-    return resp.json();
-  })
-  .then((data) => {
-    console.log(data);
-    // Title - console.log(data.Title);
-    // Release yearconsole.log(data.Year);
-    // Runtime in minutes console.log(data.Runtime);
-    // Age rating console.log(data.Rated);
-    // ImdbRating console.log(data.imdbRating);
-    // Movie genreconsole.log(data.Genre);
-    // Movie plot console.log(data.Plot);
-    //Boxoffice new console.log(data.BoxOffice)
-    // Movie directors console.log(data.Director);
-    // Movie Actors console.log(data.Actors);
-    // Writers console.log(data.Writer);
-    // Poster image url console.log(data.Poster);
-  });
+searchBtn.addEventListener("click", getMovie);
+window.addEventListener("load", getMovie);
